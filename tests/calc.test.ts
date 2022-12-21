337cae73c84208b23ec506d3be76d1f0fbc95362
Carlos Ferreira <carlos@especializati.com.br>
2022-12-21 16:27:03 -0300

    test: create tests multiply

--- tests/calc.test.ts
+++ tests/calc.test.ts
@@ -1,4 +1,4 @@
-import { add } from '../src/calc'
+import { add, multiply } from '../src/calc'
 
 describe("Test CALC - add", () => {
   it("should return 15 for add(10,5)", () => {
@@ -6,5 +6,14 @@ describe("Test CALC - add", () => {
   })
   it("should return 5 for add(2,3)", () => {
     expect(add(2,3)).toBe(5)
+  })
+})
+
+describe("Test CALC - multiply", () => {
+  it("should return 20 for multiply(4, 5)", () => {
+    expect(multiply(4, 5)).toBe(20)
+  })
+  it("should return 50 for multiply(5, 10)", () => {
+    expect(multiply(5, 10)).toBe(50)
   })
 })
